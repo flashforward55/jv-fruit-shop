f6494f21e23cfd5039231e56b5cf4f7b7bdd4e55
Anton Stiazhkin <rudana089@gmail.com>
2025-08-02 12:35:13 +0300

    Add OperationStrategy interface to retrieve operation handlers

--- src/main/java/core/basesyntax/strategy/OperationStrategy.java
+++ src/main/java/core/basesyntax/strategy/OperationStrategy.java
@@ -1,17 +1,8 @@
 package core.basesyntax.strategy;
 
-import core.basesyntax.model.OperationType;
+import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.OperationHandler;
-import java.util.Map;
 
-public class OperationStrategy {
-    private final Map<OperationType, OperationHandler> strategy;
-
-    public OperationStrategy(Map<OperationType, OperationHandler> strategy) {
-        this.strategy = strategy;
-    }
-
-    public OperationHandler getHandler(OperationType type) {
-        return strategy.get(type);
-    }
+public interface OperationStrategy {
+    OperationHandler get(FruitTransaction.Operation operation);
 }
