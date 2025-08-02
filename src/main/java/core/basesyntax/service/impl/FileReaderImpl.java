7cab015c4d4603f1b9d975996e3a6ba104be6b0c
Anton Stiazhkin <rudana089@gmail.com>
2025-08-02 12:39:35 +0300

    Implement file reader service using NIO

--- src/main/java/core/basesyntax/service/impl/FileReaderImpl.java
+++ src/main/java/core/basesyntax/service/impl/FileReaderImpl.java
@@ -1,20 +1,18 @@
 package core.basesyntax.service.impl;
 
-import core.basesyntax.service.ReaderService;
+import core.basesyntax.service.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-public class FileReaderServiceImpl implements ReaderService {
-
+public class FileReaderImpl implements FileReader {
     @Override
     public List<String> read(String filePath) {
         try {
             return Files.readAllLines(Path.of(filePath));
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file: " + filePath, e);
+            throw new RuntimeException("Cannot read file: " + filePath, e);
         }
     }
 }
-
