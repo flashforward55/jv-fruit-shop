54fade9312d523fe597f0cf69625a13a5b4711b2
Anton Stiazhkin <rudana089@gmail.com>
2025-08-02 12:45:27 +0300

    del files

--- src/main/java/core/basesyntax/model/Transaction.java
+++
@@ -1,3 +0,0 @@
-package core.basesyntax.model;
-
-public record Transaction(String fruit, OperationType operation, int quantity) {}
--- src/main/java/core/basesyntax/service/ReportService.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax.service;
-
-import java.util.Map;
-
-public interface ReportService {
-    void write(Map<String, Integer> report, String filePath);
-}
